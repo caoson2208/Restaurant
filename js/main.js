191ffc9b6aada2d09787d73f7279eefa55f14b31
Cao Ngọc Sơn <caongocson910@gmail.com>
2023-01-29 01:33:35 +0700

    NS commit

--- js/main.js
+++ js/main.js
@@ -26,12 +26,12 @@
   $(document).ready(function () {
     $(window).on("scroll", function () {
       if ($(this).scrollTop() > 100) {
-        $("#back-to-top").fadeIn();
+        $("#go-to-top").fadeIn();
       } else {
-        $("#back-to-top").fadeOut();
+        $("#go-to-top").fadeOut();
       }
     });
-    $("#back-to-top").click(function () {
+    $("#go-to-top").click(function () {
       $("html, body").animate({ scrollTop: 0 }, 600);
       return false;
     });
